ea1afe595df42e087b1d87b9d84d4cff50cbd16b
KANITA VERMA <vermakanita13@gmail.com>
2024-11-02 04:39:29 +0000

    Add

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,6 @@
+const accountId=144553;
+let accountEmail = "kanita@verma";
+let accountCity ="Bengaluru";
+
+// console.log(accountId);
+console.table({accountCity,accountEmail});
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let s=22;
+ s="22abh";
+ s=null
+ s=undefined
+ s=true
+ s="l"
+ s=99
+console.log(typeof(s));
+let v= Number(s);
+console.log(typeof v);
+console.log(v);
+
+let sn= String(s);
+console.log(sn);
+console.log(typeof sn)
